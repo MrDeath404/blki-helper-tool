c1a30f0724ce77d09254d452e86cfe7a12f57082
MrDeath404 <pisarekpatryk404@gmail.com>
2025-05-19 11:37:13 +0200

    Added color_untils.h

---
+++ color_untils.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+#define BLACK 16
+#define DARK_GRAY 234
+#define GRAY 240
+#define SILVER 254
+#define YELLOW
+#define ORANGE
+#define RED
+#define PINK
+#define MAGENTA
+#define PURPLE
+#define NAVY
+#define BLUE
+#define CYAN
+#define TEAL
+#define AQUA
+#define BROWN
+
+using namespace std;
+
+namespace color {
+    string set(int id) {
+
+    }
+
+    string reset() {
+
+    }
+
+    void print(string text, int color, char endl="\n") {
+
+    }
+}
+
+using color::print;
