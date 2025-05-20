ea8a0ca604fa6c21368df90c86e536a436a14c2a
MrDeath404 <pisarekpatryk404@gmail.com>
2025-05-20 09:36:50 +0200

    Update

--- color_untils.h
+++ color_untils.h
@@ -6,32 +6,34 @@
 #define DARK_GRAY 234
 #define GRAY 240
 #define SILVER 254
-#define YELLOW
-#define ORANGE
-#define RED
-#define PINK
-#define MAGENTA
-#define PURPLE
-#define NAVY
-#define BLUE
-#define CYAN
-#define TEAL
-#define AQUA
-#define BROWN
+#define WHITE 255
+#define YELLOW 11
+#define ORANGE 208
+#define RED 196
+#define PINK 213
+#define MAGENTA 207
+#define PURPLE 135
+#define NAVY 21
+#define BLUE 33
+#define CYAN 45
+#define TEAL 37
+#define AQUA 80
+#define BROWN 88
 
 using namespace std;
 
 namespace color {
-    string set(int id) {
 
+    string set(int id) {
+        return string("\e[38;5;" + to_string(id) + "m");
     }
 
     string reset() {
-
+        return string("\e[0m");
     }
 
-    void print(string text, int color, char endl="\n") {
-
+    void print(string text, int color, char endl='\n') {
+        cout << set(color) << text << reset() << endl;
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <thread>
+#include <conio.h>
 
 #ifdef __WIN32__
 #include <Windows.h>
@@ -8,34 +9,64 @@
 #include <stdlib.h>
 #endif
 
+#include "color_untils.h"
+
 using namespace std;
 
 void sleep(int milliseconds) {
     this_thread::sleep_for(chrono::milliseconds(milliseconds));
 }
 
-void main_menu(char& input) {
+void clear() {
+#ifdef __WIN32__
+    system("cls");
+#else
+    system("clear");
+#endif
+}
 
-    // menu part here
+inline char input = '0';
+inline char work_input = input;
 
-    // color change when input == section number, mean it's selected, enter will accept
+void show_option(string text, int number) {
 
-    cin >> input;
+    print("[", GRAY, '\0'); print(to_string(number), SILVER, '\0'); print("] ", GRAY, '\0');
+    print(text, work_input == to_string(number).c_str()[0] ? YELLOW : WHITE);
+}
 
-    // work here
+void main_menu() {
 
-    input = 0;
+    show_option("Frist Installation", 1);
+    show_option("Update", 2);
+    show_option("Lock Bootloader (With BlackIron)", 3);
+    show_option("Unlock Bootloader", 4);
+    show_option("Critical Unlock", 5);
+    show_option("Exit", 6);
+
+    work_input = input;
+    input = getch();
+
+    if (input == '\r') {
+        switch (work_input) {
+        case '6':
+            exit(0);
+        default:
+            break;
+        }
+    }
 }
 
 int main() {
 
-    char input;
+    clear();
 
     while (true) {
 
-        main_menu(input);
+        main_menu();
 
-        sleep(10);
+        clear();
+
+        sleep(1);
     }
 
     return 0;
